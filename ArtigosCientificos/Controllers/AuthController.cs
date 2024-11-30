ec3863f4726994f8f25fb39ae92c33ad8f772fde
David Capa <capaseitadavid@gmail.com>
2024-11-30 17:34:26 +0000

    Added Auth Controller and Jwt Service

---
+++ ArtigosCientificos/Controllers/AuthController.cs
@@ -0,0 +1,53 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+
+using ArtigosCientificos.Api.JWTService;
+using ArtigosCientificos.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ArtigosCientificos.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new User();
+        private readonly IConfiguration configuration;
+        public AuthController(IConfiguration _configuration)
+        {
+            this.configuration = _configuration;    
+        }
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDTO userDTO)
+        {
+            if (userDTO == null)
+            {
+                return BadRequest("Invalid data");
+            }
+            user.Username = userDTO.Username;
+            user.PasswordHash = BCrypt.Net.BCrypt.HashPassword(userDTO.Password);
+            return Ok(user);
+        }
+
+
+        [HttpPost("login")]
+        public ActionResult<User> Login(UserDTO userDTO)
+        {
+
+            if (user.Username != userDTO.Username)
+            {
+                return BadRequest("Invalid username");
+            }
+            if (!BCrypt.Net.BCrypt.Verify(userDTO.Password, user.PasswordHash))
+            {
+                return BadRequest("Invalid password");
+            }
+            var token = new Jwt(this.configuration).CreateToken(user);
+            return Ok(token);
+        }
+
+
+
+    }
+}
---
+++ ArtigosCientificos/JWTService/Jwt.cs
@@ -0,0 +1,39 @@
+﻿using ArtigosCientificos.Api.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ArtigosCientificos.Api.JWTService
+{
+    public class Jwt
+    {
+
+        private readonly IConfiguration configuration;
+
+        public Jwt(IConfiguration configuration) {
+
+            this.configuration = configuration;
+        }
+
+        public string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(this.configuration.GetSection("AppSettings:Token").Value));
+            SigningCredentials creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            JwtSecurityToken token = new JwtSecurityToken(
+                issuer: this.configuration.GetSection("AppSettings:Issuer").Value,
+                audience: this.configuration.GetSection("AppSettings:Audience").Value,
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
