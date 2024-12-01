c28354231c5bbd6d9866d8f3ae04c08d04f67d30
David Capa <capaseitadavid@gmail.com>
2024-12-01 15:10:50 +0000

    EF Implementation

---
+++ ArtigosCientificos/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using ArtigosCientificos.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ArtigosCientificos.Api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<User>? Users { get; set; }
+    }
+}
